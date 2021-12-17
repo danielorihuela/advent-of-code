b65b3562c14df025484f94c0ebb4e4301d350978
dani <danielorihuelarodriguez@gmail.com>
2021-12-17 23:00:01 +0100

    feat: first puzzle of eleventh day 2021

---
+++ 2021/puzzle-11a/src/main.rs
@@ -0,0 +1,76 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut matrix: Vec<Vec<u8>> = reader
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .chars()
+                .map(|x| x.to_digit(10).unwrap() as u8)
+                .collect::<Vec<u8>>()
+        })
+        .collect();
+
+    let mut total_flashes = 0;
+    for _ in 0..100 {
+        let height = matrix.len();
+        let width = matrix[0].len();
+        for i in 0..height {
+            for j in 0..width {
+                matrix[i][j] += 1;
+                if matrix[i][j] == 10 {
+                    flash(&mut matrix, i, j);
+                }
+            }
+        }
+
+        let mut triggered_flashes = 0;
+        for i in 0..height {
+            for j in 0..width {
+                if matrix[i][j] > 9 {
+                    triggered_flashes += 1;
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        total_flashes += triggered_flashes;
+    }
+
+    println!("{}", total_flashes);
+}
+
+fn flash(matrix: &mut Vec<Vec<u8>>, row: usize, column: usize) {
+    let row = row as i8;
+    let column = column as i8;
+    let indexes = [
+        (row - 1, column - 1),
+        (row - 1, column),
+        (row - 1, column + 1),
+        (row, column + 1),
+        (row + 1, column + 1),
+        (row + 1, column),
+        (row + 1, column - 1),
+        (row, column - 1),
+    ];
+
+    for (row, column) in indexes {
+        if position_inside_grid(matrix, row, column) && matrix[row as usize][column as usize] < 10 {
+            matrix[row as usize][column as usize] += 1;
+            if matrix[row as usize][column as usize] == 10 {
+                flash(matrix, row as usize, column as usize);
+            }
+        }
+    }
+}
+
+fn position_inside_grid(matrix: &Vec<Vec<u8>>, row: i8, column: i8) -> bool {
+    if row < 0 || row > matrix.len() as i8 - 1 || column < 0 || column > matrix[0].len() as i8 - 1 {
+        return false;
+    }
+
+    true
+}
