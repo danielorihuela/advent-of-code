24fe97ec1e3be9f7f191dc9b5ddcabdc157f6a34
dani <danielorihuelarodriguez@gmail.com>
2023-12-19 15:54:46 +0100

    feat: aoc 2023 puzzle 11

---
+++ 2023/puzzle-11b/src/main.rs
@@ -0,0 +1,86 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    fs,
+};
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let matrix = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+
+    // get galaxies
+    let mut galaxies = vec![];
+    for i in 0..matrix.len() {
+        for j in 0..matrix[0].len() {
+            if matrix[i][j] == '#' {
+                galaxies.push((j, i));
+            }
+        }
+    }
+
+    // bfs for every pair of galaxies
+    let mut result = 0u128;
+    for i in 0..galaxies.len() - 1 {
+        let mut visited = HashSet::new();
+        let mut visited_galaxies = 0;
+
+        let mut to_visit = VecDeque::new();
+        to_visit.push_back((galaxies[i], 0));
+
+        while visited_galaxies != galaxies[i + 1..].len() {
+            let ((x, y), parent_value) = to_visit.pop_front().unwrap();
+            if visited.get(&(x, y)).is_some() {
+                continue;
+            }
+
+            let mut value = parent_value + 1;
+            if x < matrix[0].len() - 1 {
+                let mut all_dots = true;
+                for i in 0..matrix.len() {
+                    if matrix[i][x] != '.' {
+                        all_dots = false;
+                    }
+                }
+                if all_dots {
+                    value = parent_value + 1000000;
+                }
+                to_visit.push_back(((x + 1, y), value));
+            }
+            if y < matrix.len() - 1 {
+                if matrix[y].iter().all(|c| c == &'.') {
+                    value = parent_value + 1000000;
+                }
+                to_visit.push_back(((x, y + 1), value));
+            }
+            if x > 0 {
+                let mut all_dots = true;
+                for i in 0..matrix.len() {
+                    if matrix[i][x] != '.' {
+                        all_dots = false;
+                    }
+                }
+                if all_dots {
+                    value = parent_value + 1000000;
+                }
+                to_visit.push_back(((x - 1, y), value));
+            }
+            if y > 0 {
+                if matrix[y].iter().all(|c| c == &'.') {
+                    value = parent_value + 1000000;
+                }
+                to_visit.push_back(((x, y - 1), value));
+            }
+
+            visited.insert((x, y));
+
+            if galaxies[i + 1..].contains(&(x, y)) {
+                visited_galaxies += 1;
+                result += value - 1;
+            }
+        }
+    }
+    println!("{}", result);
+}
